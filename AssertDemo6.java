9166bed509001d0f0c06e03dae336a9c2c696dfc
PrabudeepSingh <prabhudeepsb@gmail.com>
2017-11-26 23:09:38 +0530

    To illustrate the concept of Assertions in java.

---
+++ AssertDemo6.java
@@ -0,0 +1,12 @@
+class j
+{
+	public static void main(String[] args) 
+	{
+		boolean assertOn = false;
+
+		assert(assertOn):"Assert On";
+
+		if(assertOn == false)
+			System.out.println("Assert Off");
+	}
+}
